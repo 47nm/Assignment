376a13fb2265e3ed4a5c7f1d5183e535987a8c15
Multani <mulna01@ca.com>
2017-09-11 15:05:45 +0530

    refactor util

--- Assignment/Util.cpp
+++ Assignment/Util.cpp
@@ -1,4 +1,5 @@
 #include "Util.h"
+//!definition of Color members
 Color::Color() {
 	red = blue = green = 0;
 }
@@ -10,6 +11,41 @@ Color::Color(GLubyte r, GLubyte g, GLubyte b) {
 }
 
 
+//!definition of Point members
+Point::Point() {
+	x = y = 0;
+}
+Point::Point(int _x, int _y) :x(_x), y(_y) {
+
+}
+double Point::distance(const Point& _point) {
+	return sqrt(static_cast<double>(squaredDistance(_point)));
+}
+
+long long Point::squaredDistance(const Point& _point) {
+	long long xsquare = static_cast<long long>(_point.x - x) * static_cast<long long>(_point.x - x);
+	long long ysquare = static_cast<long long>(_point.y - y) * static_cast<long long>(_point.y - y);
+	return (xsquare + ysquare);
+
+}
+
+
+//!definition of Line members
+Line::Line() {
+}
+Line::Line(Point _start, Point _end) :start(_start), end(_end) {
+}
+double Line::length() {
+	return start.distance(end);
+}
+
+//!definition of Circle members
+Circle::Circle() :radius(0) {
+}
+Circle::Circle(Point p, int r) :center(p), radius(r) {
+}
+
+//!definition of GridPoint members
 void GridPoint::setOn() {
 	state = STATE_ON;
 }
@@ -19,11 +55,9 @@ void GridPoint::setOff() {
 void GridPoint::toggle() {
 	state = !state;
 }
-
 GridPoint::GridPoint() {
 	state = GridPoint::STATE_OFF;
 }
-
 GridPoint::GridPoint(bool _state, int _x, int _y) {
 	x = _x;
 	y = _y;
--- Assignment/Util.h
+++ Assignment/Util.h
@@ -23,28 +23,15 @@ public:
 class Point {
 public:
 	int x, y;
-	Point() {
-		x = y = 0;
-	}
-	Point(int _x, int _y) :x(_x), y(_y) {
-
-	}
-	double distance(const Point& _point) {
-		return sqrt(static_cast<double>(squaredDistance(_point)));
-	}
-	long long squaredDistance(const Point& _point) {
-		long long xsquare = static_cast<long long>(_point.x - x) * static_cast<long long>(_point.x - x);
-		long long ysquare = static_cast<long long>(_point.y - y) * static_cast<long long>(_point.y - y);
-		return (xsquare + ysquare);
-		
-	}
+	Point();
+	Point(int _x, int _y);
+	double distance(const Point& _point);
+	long long squaredDistance(const Point& _point);
 };
 
 //represents a grid point.
 class GridPoint: public Point{
-	// there is no invariant on point hence no getter and setters
-
-
+	
 public:
 	bool state;
 	const static bool STATE_ON = true;
@@ -61,23 +48,17 @@ public:
 class Line {
 public:
 	Point start, end;
-	Line(){
-	}
-	Line(Point _start, Point _end) :start(_start), end(_end) {
-	}
-	double length() {
-		return start.distance(end);
-	}
+	Line();
+	Line(Point _start, Point _end);
+	double length();
 };
 
 class Circle {
 public:
 	Point center;
 	int radius;
-	Circle():radius(0){
-	}
-	Circle(Point p, int r):center(p),radius(r) {
-	}
+	Circle();
+	Circle(Point p, int r);
 
 };
 #endif
